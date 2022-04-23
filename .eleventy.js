cc71b90bd306114c83542bbcec904d73ef37b088
geofftobin71 <geofftobin71@gmail.com>
2022-04-23 04:54:30 +0000

    reviews slider

--- .eleventy.js
+++ .eleventy.js
@@ -73,6 +73,16 @@ module.exports = eleventyConfig => {
     return content;
   });
 
+  eleventyConfig.addFilter("shuffle", (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  });
+
   eleventyConfig.addFilter("cssmin", (code) => {
     if(site.dev) { return code; }
 
