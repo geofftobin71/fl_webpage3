78bbd00965a296c84fbde293c31efedf90feeb5c
geofftobin71 <geofftobin71@gmail.com>
2022-04-28 06:54:55 +0000

    forestry setup

--- src/_data/instagram_gallery.js
+++ src/_data/instagram_gallery.js
@@ -2,19 +2,9 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
 
-  const token = process.env.INSTAGRAM_TOKEN;
+  const json = await EleventyFetch("http://dev.floriade.co.nz/php/instagram.php",
+    {duration:"1d",type:"json",verbose:true});
 
-  if(token) {
-    EleventyFetch('https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"4w",type:"json",removeUrlQueryParams:true});
-    /* .then(json => console.log(json)); */
-
-    const json = await EleventyFetch('https://graph.instagram.com/me/media?fields=id,media_url,thumbnail_url,caption,timestamp&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"1d",type:"json",removeUrlQueryParams:true,verbose:true});
-
-    // const data = await json.data;
-
-    return json.data;
-  } else {
-    return [];
-  }
+  return json.data;
 };
 
