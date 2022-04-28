c54b7d43046d1853e292b7d0a5ac8f1e97d67e99
geofftobin71 <geofftobin71@gmail.com>
2022-04-28 02:41:10 +0000

    forestry setup

--- src/_data/instagram_gallery.js
+++ src/_data/instagram_gallery.js
@@ -2,13 +2,19 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
 
-  EleventyFetch('https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"4w",type:"json",removeUrlQueryParams:true});
-  /* .then(json => console.log(json)); */
+  const token = process.env.INSTAGRAM_TOKEN;
 
-  const json = await EleventyFetch('https://graph.instagram.com/me/media?fields=id,media_url,thumbnail_url,caption,timestamp&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"1d",type:"json",removeUrlQueryParams:true,verbose:true});
+  if(token) {
+    EleventyFetch('https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"4w",type:"json",removeUrlQueryParams:true});
+    /* .then(json => console.log(json)); */
 
-  // const data = await json.data;
+    const json = await EleventyFetch('https://graph.instagram.com/me/media?fields=id,media_url,thumbnail_url,caption,timestamp&access_token=' + process.env.INSTAGRAM_TOKEN, {duration:"1d",type:"json",removeUrlQueryParams:true,verbose:true});
 
-  return json.data;
+    // const data = await json.data;
+
+    return json.data;
+  } else {
+    return [];
+  }
 };
 
